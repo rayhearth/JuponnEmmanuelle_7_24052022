1edc8281cf61be0ac14f83f521914eca2d498955
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-07-08 14:33:05 +0200

    concat item

--- assets/scripts/algo2.js
+++ assets/scripts/algo2.js
@@ -21,46 +21,47 @@ let Search = () => {
 
   const SearchArray = ['name', 'description']
 
-
-  /***********************
-  TEST
-   ****************/
   //flag peut être =-1 ou undefined--va permettre d'itérer sur un un array pour le spread l42
   let flag = -1
   //array vide d'ingredients
   let ing = []
-  let app=[]
+  let app = []
   let ust = []
-  
+
   recipes.forEach((element, index) => {
     //si flag diff de index
     if (flag != index) {
       //alors on stocke index de l'ing ds un array
       ing[index] = []
       app[index] = []
-      ust [index] = []
+      ust[index] = []
       flag = index
     }
     //pour chaque item on obtient un array avec l'index de la recipe et les items de cette recipe
     ing[index] = [...ing[index], ...element.ingredients.map(i => i.ingredient)]
     app[index] = [...app[index], element.appliance]
     ust[index] = [...ust[index], element.ustensils.toString()]
+
+    let ingArray = ing[index]
+    let appArray = app[index]
+    let ustArray = ust[index]
+
+    let itemArray = [...new Set(ingArray.concat(appArray).concat(ustArray))]
+    // console.log(itemArray)
   })
-  // console.log(ing)
-  // console.log(app)
-  console.log(ust)
+
 
   const research = () => {
     if (searchValue >= 3) {
-      let searchArray = recipes.reduce(function (accumulator, currentValue) {
-        if (accumulator.find(currentValue)) {
-          accumulator.push(currentValue)
+      let searchArray = recipes.reduce(function (accumulator, searchValue) {
+        if (accumulator.find(searchValue)) {
+          accumulator.push(searchArray)
+          console.log(searchArray)
         }
         return accumulator
       }, [])
 
 
-      console.log(searchArray)
     }
 
   }
