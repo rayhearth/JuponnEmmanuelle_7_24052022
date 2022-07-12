fd017f375d1d31427459cedcf0b443e37c5680a8
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-07-12 14:16:57 +0200

    filter

--- assets/scripts/algo2.js
+++ assets/scripts/algo2.js
@@ -23,16 +23,16 @@ let Search = () => {
 
 
 
-  recipes.forEach(function(item, index, array) {
-    console.log(index, item)
-  })
+  // recipes.forEach(function (item, index, array) {
+  //   console.log(index, item)
+  // })
 
 
-  recipes.forEach(function(item, index, array) {
-    item.forEach(function(itm, idx, arr) {
-      console.log(index, idx, itm)
-    })
-  })
+  // recipes.forEach(function (item, index, array) {
+  //   item.forEach(function (itm, idx, arr) {
+  //     console.log(index, idx, itm)
+  //   })
+  // })
 
 
 
@@ -40,23 +40,19 @@ let Search = () => {
 
   recipes.forEach(element => {
     //si flag diff de index
-  
+
     if (searchValue.length >= 3) {
       // let result = element.toString().includes(searchValue)
-  
+
       if (element.name.toLowerCase().includes(searchValue) || element.description.includes(searchValue) || element.ingredients.concat(ingredients).includes(searchValue)) {
         searchArray.push(element)
       }
 
-      //0 ["apple", "orange", "mango"]
-      //1 ["rose", "lotus", "lily"]
-      //2 ["cabbage", "carrot", "beans"]
-      
-      myArray.forEach(function(item, index, array) {
-        item.forEach(function(itm, idx, arr) {
-          console.log(index, idx, itm);
-        });
-      });
+      // recipes.forEach(function (item, index, array) {
+      //   item.forEach(function (itm, idx, arr) {
+      //     console.log(index, idx, itm);
+      //   });
+      // });
 
 
 
@@ -64,20 +60,20 @@ let Search = () => {
 
 
 
-  
-       /* Création des recipes cards en fonction du resultat de la recherche*/
-            //on parcour l'array obtenu et on instancie notre class recipeCard
-            const results = searchArray.map(s => new RecipeCard(s))
-            // console.log(searchArray)
-            //on cree une méthode qui va gérer le html de toutes les ecttes trouvées
-            const visualAll = (searchArray) => {
-                let all = ''
-                for (let searching of searchArray) {
-                    all += searching.renderRecipe()
-                }
-                return all
-            }
-            document.querySelector('#recettes').innerHTML = visualAll(results)
+
+      /* Création des recipes cards en fonction du resultat de la recherche*/
+      //on parcour l'array obtenu et on instancie notre class recipeCard
+      const results = searchArray.map(s => new RecipeCard(s))
+      // console.log(searchArray)
+      //on cree une méthode qui va gérer le html de toutes les ecttes trouvées
+      const visualAll = (searchArray) => {
+        let all = ''
+        for (let searching of searchArray) {
+          all += searching.renderRecipe()
+        }
+        return all
+      }
+      document.querySelector('#recettes').innerHTML = visualAll(results)
       // itemArray.reduce(function(SearchArray,curr,result){
       //   if (curr === searchValue)
       //   SearchArray.push(result)
@@ -87,39 +83,40 @@ let Search = () => {
   })
 
   //flag peut être =-1 ou undefined--va permettre d'itérer sur un un array pour le spread l42
-  // let flag = -1
-  // //array vide d'ingredients
-  // let ing = []
-  // let app = []
-  // let ust = []
+  let flag = -1
+  //array vide d'ingredients
+  let ing = []
+  let app = []
+  let ust = []
 
-  // recipes.forEach((element, index) => {
-  //   //si flag diff de index
-  //   if (flag != index) {
-  //     //alors on stocke index de l'ing ds un array
-  //     ing[index] = []
-  //     app[index] = []
-  //     ust[index] = []
-  //     flag = index
-  //   }
-  //   //pour chaque item on obtient un array avec l'index de la recipe et les items de cette recipe
-  //   ing[index] = [...ing[index], ...element.ingredients.map(i => i.ingredient)]
-  //   app[index] = [...app[index], element.appliance]
-  //   ust[index] = [...ust[index], element.ustensils.toString()]
-    
-  //   //on concat les trois tableaux et on enleve les doublons
-  //   let itemArray = [...new Set(ing[index].concat(app[index]).concat(ust[index]).concat(element.name).concat(element.description))]
-    
-  //   if(searchValue.length >= 3) {
-  //     itemArray.reduce(function(SearchArray,curr,result){
-  //       if (curr === searchValue)
-  //       SearchArray.push(result)
-  //     })
-  //     console.log(SearchArray)
-  //     }
+  recipes.forEach((element, index) => {
+    //si flag diff de index
+    if (flag != index) {
+      //alors on stocke index de l'ing ds un array
+      ing[index] = []
+      app[index] = []
+      ust[index] = []
+      flag = index
+    }
+    //pour chaque item on obtient un array avec l'index de la recipe et les items de cette recipe
+    ing[index] = [...ing[index], ...element.ingredients.map(i => i.ingredient)]
+    app[index] = [...app[index], element.appliance]
+    ust[index] = [...ust[index], element.ustensils.toString()]
 
-  //   })
-    
+    //on concat les trois tableaux et on enleve les doublons
+    let itemArray = [...new Set(ing[index].concat(app[index]).concat(ust[index]).concat(element.name).concat(element.description))]
+
+    if (searchValue.length >= 3) {
+      itemArray.filter((item, index) => {
+        if (item.includes(searchValue)) {
+          searchArray.push(item, index)
+          console.log(searchArray)
+        }
+      })
+    }
+
+  })
+
 }
 
 
