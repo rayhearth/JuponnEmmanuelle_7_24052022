1291ccbefecd98c555f601e7d14a0f95dce2c1ba
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2022-07-20 18:27:21 +0200

    correctif erreur saisie

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,8 @@ import { RecipeCard } from './models/recipeCard.js'
 
 //DOM
 const recipesContainer = document.querySelector('#recettes')
+//const ou est stocke la div no result
+const ghost = document.querySelector('#noResults')
 const tags = document.querySelector('#tagsSelect')
 const tagsList = document.querySelector('#tagsList')
 
@@ -21,6 +23,7 @@ const ustLabel = document.querySelector('#ustL')
 const ustSearch = document.querySelector('#ustensiles')
 
 const principalSearch = document.querySelector('#search')
+let searchValue = ''
 
 //ul
 const listOfIngredients = document.querySelector('#ingredientsList')
@@ -259,6 +262,67 @@ const startTagsListener = () => {
     }
 }
 window.addEventListener('load', displayTags)
+
+
+let Search = () => {
+
+    searchValue = principalSearch.value.trim().toLowerCase()
+    let searchArray = []
+
+    if (searchValue.length >= 3) {
+        for (let i = 0; i < recipes.length; i++) {
+            if (recipes[i].name.toLowerCase().includes(searchValue) || recipes[i].description.includes(searchValue)) {
+                searchArray.push(recipes[i])
+            } else {
+                for (let r = 0; r < recipes[i].ingredients.length; r++) {
+                    if (recipes[i].ingredients[r].ingredient.toLowerCase().includes(searchValue)) {
+                        searchArray.push(recipes[i])
+                        break;
+                    }
+                }
+            }
+            // recipes.splice(0,recipes.length,...searchArray)
+            // console.log(recipes)
+
+            /*MAJ des liste en fonction du résultat de la recherche principale*/
+            let ingFiltered = searchArray.map(i => i.ingredients.map(n => n.ingredient.toLowerCase()))
+            ingFiltered = ingFiltered + ''
+            let iFiltredData = ingFiltered.split(',')
+            let newIngredientArray = [...new Set(iFiltredData)].sort()
+
+            let appFiltered = searchArray.map(a => a.appliance.toLowerCase())
+            let newApplianceArray = [...new Set(appFiltered)].sort()
+
+            let ustFistered = searchArray.map(u => u.ustensils.toString().toLowerCase())
+            ustFistered = ustFistered + ''
+            let uFilteredData = ustFistered.split(',')
+            let newUstensilsArray = [...new Set(uFilteredData)].sort()
+
+            /* Création des recipes cards en fonction du resultat de la recherche*/
+            //on parcour l'array obtenu et on instancie notre class recipeCard
+            const results = searchArray.map(s => new RecipeCard(s))
+            // console.log(searchArray)
+            //on cree une méthode qui va gérer le html de toutes les ecttes trouvées
+            const visualAll = (searchArray) => {
+                let all = ''
+                for (let searching of searchArray) {
+                    all += searching.renderRecipe()
+                }
+                return all
+            }
+            document.querySelector('#recettes').innerHTML = visualAll(results)
+            //s'il n'y a pas de recette on fait app le mess no result sinon il est caché
+            if (searchArray.length == 0) {
+                ghost.classList.remove('hidden')
+            } else {
+                ghost.classList.add('hidden')
+            }
+        }
+    }
+}
+
+principalSearch.addEventListener('input', Search)
+
 
 //on injecte le html du render recipe ds notre section recette
 const displayRecipes = () => {
